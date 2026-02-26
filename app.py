391c0c7547cf86e293a0323d9be107860c8d069e
Meti2021 <82475924+Meti2021@users.noreply.github.com>
2026-02-26 14:06:59 -0500

    Update app.py
    app background color changed to white

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Expense Analytics Dashboard", layout="wide")
 st.markdown("""
 <style>
 .stApp {
-    background-color: #eaf4ff;
+    background-color: white;
 }
 
 .main-title {
@@ -162,3 +162,4 @@ if uploaded_file:
             st.success("🎯 Excellent insight! This is your highest spending category. Smart financial awareness! 💡💰")
         else:
             st.warning("📊 Not the highest category. Review the dashboard and try again!")
+
